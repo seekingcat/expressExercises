a91e40e05cdbcc0e327540c2c3737b565b1c72c4
seekingcat <llana_a154@yahoo.co.uk>
2024-06-01 10:38:35 +0100

    exercise files for ejs exercises

--- templateDemo/index.js
+++ templateDemo/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const app = express();
 const path = require('path');
+const redditData = require('./data.json')
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'))
+app.set('views', path.join(__dirname, '/views'));
+
 
 app.get('/', (req, res) => {
     res.render('home')
@@ -12,12 +16,22 @@ app.get('/', (req, res) => {
 
 app.get('/r/:subreddit', (req, res) => {
     const {subreddit} = req.params;
-    res.render('subreddit', {subreddit})
+    const data = redditData[subreddit];
+    if (data) {
+    res.render('subreddit', {...data})
+    } else {
+        res.render('notfound', {subreddit})
+    }
 })
 
 app.get('/random', (req, res) => {
     const num = Math.floor(Math.random() * 10) + 1;
     res.render('random', {num})
+})
+
+app.get('/cats', (req, res) => {
+    const cats = ['Cosmo', 'Sugar', 'Catdog', 'Percy', 'Gubbins'];
+    res.render('cats', {cats})
 })
 
 app.listen(3000, () => {
