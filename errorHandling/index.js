843ae39846179402fdf8a858bfd48319c2e206e3
seekingcat <llana_a154@yahoo.co.uk>
2024-06-19 14:24:19 +0100

    more error handling syntax handling

--- errorHandling/index.js
+++ errorHandling/index.js
@@ -20,41 +20,59 @@ const verifyPassword = (req, res, next) => {
     if (password === 'chickennugget') {
         next();
     } else {
-        res.send('SORRY YOU NEED A PASSWORD')
+        // res.send('SORRY YOU NEED A PASSWORD')
+        res.status(401);
+        throw ('PASSWORD REQUIRED')
     }
 }
+
+
+
+
+
 // app.use((req, res, next) => {
-//     console.log('FIRST MIDDLEWARE!!!');
-//     return next();
-//     console.log('FIRST MIDDLEWARE AFTER CALLING NEXT!!!!')  // this will not print now that we have the return keyword
-// })
-// app.use((req, res, next) => {
-//     console.log('SECOND MIDDLEWARE!!!');
-//     return next();
-// })
-// app.use((req, res, next) => {
-//     console.log('THIRD MIDDLEWARE!!!');
-//     return next();
-// })
+    //     console.log('FIRST MIDDLEWARE!!!');
+    //     return next();
+    //     console.log('FIRST MIDDLEWARE AFTER CALLING NEXT!!!!')  // this will not print now that we have the return keyword
+    // })
+    // app.use((req, res, next) => {
+        //     console.log('SECOND MIDDLEWARE!!!');
+        //     return next();
+        // })
+        // app.use((req, res, next) => {
+            //     console.log('THIRD MIDDLEWARE!!!');
+            //     return next();
+            // })
+            
+            app.get('/', (req, res) => {
+                console.log(`REQUEST DATE: ${req.requestTime}`);
+                res.send('HOMEPAGE')
+            })
+            
+            app.get('/error', (req, res) => {
+                chicken.fly();
+            })
+            
+            app.get('/dogs', (req, res) => {
+                console.log(`REQUEST DATE: ${req.requestTime}`);
+                res.send('WOOF WOOF!!')
+            })
+            
+            app.get('/secret', verifyPassword, (req, res) => {
+                res.send('MY SECRET IS: I work in theatre but I do not like talking to people!')
+            })
+            
+            app.use((req, res) => {
+                res.status(404).send('NOT FOUND')
+            })
 
-app.get('/', (req, res) => {
-    console.log(`REQUEST DATE: ${req.requestTime}`);
-    res.send('HOMEPAGE')
-})
-
-app.get('/dogs', (req, res) => {
-    console.log(`REQUEST DATE: ${req.requestTime}`);
-    res.send('WOOF WOOF!!')
-})
-
-app.get('/secret', verifyPassword, (req, res) => {
-    res.send('MY SECRET IS: I work in theatre but I do not like talking to people!')
-})
-
-// app.use((req, res) => {
-//     res.status(404).send('NOT FOUND')
-// })
-
-app.listen(3000, () => {
-    console.log('App running on Port 3000')
+            app.use((err, req, res, next) => {
+                console.log("***********************************")
+                console.log("*************ERROR ************")
+                console.log("***********************************")
+                next(err);
+            })
+            
+            app.listen(3000, () => {
+                console.log('App running on Port 3000')
 })
