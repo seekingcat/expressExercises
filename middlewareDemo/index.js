5bc7cf6da9fb0738cb470423a3f530e535cd2c1c
seekingcat <llana_a154@yahoo.co.uk>
2024-06-19 12:30:13 +0100

    new exercise files for error handling

---
+++ middlewareDemo/index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+
+// morgan('tiny');
+app.use(morgan('dev'))
+
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    next();
+})
+
+app.use('/dogs', (req, res, next) => {
+    console.log('I LOVE DOGS');
+    next();
+})
+
+const verifyPassword = (req, res, next) => {
+    const {password} = req.query;
+    if (password === 'chickennugget') {
+        next();
+    } else {
+        res.send('SORRY YOU NEED A PASSWORD')
+    }
+}
+// app.use((req, res, next) => {
+//     console.log('FIRST MIDDLEWARE!!!');
+//     return next();
+//     console.log('FIRST MIDDLEWARE AFTER CALLING NEXT!!!!')  // this will not print now that we have the return keyword
+// })
+// app.use((req, res, next) => {
+//     console.log('SECOND MIDDLEWARE!!!');
+//     return next();
+// })
+// app.use((req, res, next) => {
+//     console.log('THIRD MIDDLEWARE!!!');
+//     return next();
+// })
+
+app.get('/', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
+    res.send('HOMEPAGE')
+})
+
+app.get('/dogs', (req, res) => {
+    console.log(`REQUEST DATE: ${req.requestTime}`);
+    res.send('WOOF WOOF!!')
+})
+
+app.get('/secret', verifyPassword, (req, res) => {
+    res.send('MY SECRET IS: I work in theatre but I do not like talking to people!')
+})
+
+// app.use((req, res) => {
+//     res.status(404).send('NOT FOUND')
+// })
+
+app.listen(3000, () => {
+    console.log('App running on Port 3000')
+})
